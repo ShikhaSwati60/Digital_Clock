7e792f4fe7c9cec64c5f1ca4a5de4758b143f074
Swati Shikha <72434791+ShikhaSwati60@users.noreply.github.com>
2022-06-30 14:35:20 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,16 +1,24 @@
-let a, date, time;
+let a;
+let date;
+let time;
 var am_pm = 'AM';
+
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+
+//arrow function
 setInterval(() => {
     a = new Date();
     date = a.toLocaleDateString(undefined, options);
     let hrs = a.getHours().toString();
     let min = a.getMinutes().toString();
     let sec = a.getSeconds().toString();
+    
     if (hrs > 12)
         am_pm = 'PM';
     else
         am_pm = 'AM';
+    
     time = hrs.padStart(2, '0') + ":" + min.padStart(2, '0') + ":" + sec.padStart(2, '0') + " " + am_pm;
     document.getElementById('time').innerHTML = time + "<br> on " + date;
-}, 1000);
+},
+  1000);
